39c2c6c67bc9a3e9d48bed225969b64f174f3908
buscaroli <m.buscaroli@outlook.com>
2020-12-06 15:16:49 +0000

    Added Input Type to the mutations to improve the structure of the app; the mutation definitions are simpler and the input types can be reused across operations.

--- src/index.js
+++ src/index.js
@@ -75,9 +75,28 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!, 
+        email: String!, 
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!, 
+        body: String!, 
+        published: Boolean!, 
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!, 
+        author: ID!, 
+        post: ID!
     }
 
 
@@ -148,14 +167,14 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, ctx, info) {
-            const emailTaken = users.some(user => user.email === args.email)
+            const emailTaken = users.some(user => user.email === args.data.email)
 
             if (emailTaken) {
                 throw new Error ('Email already taken.')
             }
             const user = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             users.push(user)
@@ -163,13 +182,13 @@ const resolvers = {
             return user
         },
         createPost(parent, args, ctx, info) {
-            const userExists = users.some(user => user.id === args.author)
+            const userExists = users.some(user => user.id === args.data.author)
 
             if (!userExists) throw new Error ('User doesnt Exist.')
 
             const post = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             posts.push(post)
@@ -177,15 +196,15 @@ const resolvers = {
             return post
         },
         createComment(parent, args, ctx, info) {
-            const userExists = users.some(user => user.id === args.author)
+            const userExists = users.some(user => user.id === args.data.author)
             if (!userExists) throw new Error ('Couldnt find author.')
 
-            const postExistsAndPublished = posts.some(post => post.id === args.post &&  post.published)
+            const postExistsAndPublished = posts.some(post => post.id === args.data.post &&  post.published)
             if (!postExistsAndPublished) throw new Error ('Couldnt find a published post.')
 
             const comment = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             comments.push(comment)
